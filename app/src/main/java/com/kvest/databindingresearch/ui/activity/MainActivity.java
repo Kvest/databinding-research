31e7b2758044ff659df273e14edf65aaaa9fce18
Roman Bielokon <roman_slavyansk@mail.ru>
2016-01-18 13:21:14 +0200

    Initial commit

---
+++ app/src/main/java/com/kvest/databindingresearch/ui/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kvest.databindingresearch.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kvest.databindingresearch.R;
+
+/**
+ * Created by roman on 1/18/16.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
